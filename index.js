64b2f1c4398608316b2b38f78c6617b951f23d94
Vishal Gaurav <lemeraemail@gmail.com>
2022-03-31 11:33:53 +0530

    fix: add JS code in comment.

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ const card = document.getElementById("card-dismiss");
 const dismissButton = document.querySelector(".card-dismiss-icon");
 
 dismissButton.addEventListener("click", () => {
-  console.log("clicked");
   card.style.display = "none";
 });
